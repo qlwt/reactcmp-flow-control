9d2a0769d7adea6638f6d671018e3d641203402d
qte <>
2025-10-26 16:35:53 +0200

    add lazy children option for CmpIf

--- src/component/if/element/index.tsx
+++ src/component/if/element/index.tsx
@@ -1,14 +1,18 @@
 import * as r from "react"
 
 export type CmpIf_Props = {
-    readonly children?: r.ReactNode
     readonly fallback?: () => r.ReactNode
     readonly value: null | undefined | false | any
+    readonly children?: r.ReactNode | (() => r.ReactNode)
 }
 
 export const CmpIf: r.FC<CmpIf_Props> = r.memo(props => {
     if (props.value === null || props.value === undefined || props.value === false) {
         return props.fallback?.() || null
+    }
+
+    if (typeof props.children === "function") {
+        return props.children()
     }
 
     return props.children
